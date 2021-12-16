27c4a57566be49d972c2efc430db1b20ebe673dd
Raj Singh <rajkumar@technogramsolutions.com>
2021-12-16 12:03:31 +0530

    Get and Display User system Details

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, Fragment } from 'react';
+import Loader from './loader';
 
 function App() {
+  const [data, setData] = useState({});
+  const [loader, setLoader] = useState(false);
+
+  const getDataHandler = async () => {
+    setLoader(true)
+    await fetch('https://geolocation-db.com/json/')
+      .then(res => res.json())
+      .then(result => {
+        setLoader(false)
+        setData(result)
+      });
+  }
+
+  const { IPv4 = '', city = '',
+    country_code = '', country_name = '',
+    state = '', postal = '', latitude = '',
+    longitude = '' } = data;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="wrapper_bg">
+          <button
+            onClick={() => getDataHandler()}
+            className="search_button"
+          >
+            Get User Details
+          </button>
+          {loader ? <Loader /> :
+            <div className="table-responsive">
+              {Object.keys(data).length > 0 &&
+                <table id="customers">
+                  <thead>
+                    <tr>
+                      <th>IPv4</th>
+                      <th>City</th>
+                      <th>Country Code</th>
+                      <th>Country Name</th>
+                      <th>State</th>
+                      <th>Postal Code</th>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{IPv4}</td>
+                      <td>{city}</td>
+                      <td>{country_code}</td>
+                      <td>{country_name}</td>
+                      <td>{state}</td>
+                      <td>{postal}</td>
+                      <td>{latitude}</td>
+                      <td>{longitude}</td>
+                    </tr>
+                  </tbody>
+                </table>}
+            </div>
+          }
+        </div>
       </header>
     </div>
   );
---
+++ src/loader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader = () => {
+    return (
+        <div class="loader">
+            <div class="box">
+                <div class="circle"></div>
+            </div>
+            <div class="box">
+                <div class="circle"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
